c688723c91569cfea224e950cbbca61bd290d9e7
Rémy Raes <contact@remyraes.com>
2021-12-02 15:18:09 +0100

    [test] testing enableBluetooth method instead of platformVersion

--- test/bluetooth_enable_test.dart
+++ test/bluetooth_enable_test.dart
@@ -9,7 +9,7 @@ void main() {
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+      return 'true';
     });
   });
 
@@ -17,7 +17,7 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await BluetoothEnable.platformVersion, '42');
+  test('getEnableBluetoothResult', () async {
+    expect(await BluetoothEnable.enableBluetooth, 'true');
   });
 }
